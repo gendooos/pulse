aa4c0d3c8aff714125b65c784cbe2687dde3c4a3
Gennadiy <Gendooos@gmail.com>
2021-04-29 16:40:23 +0500

    add modals

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,19 @@ $(document).ready(function () {
 
     toogleSlide('.catalog-item__link');
     toogleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click',function(){
+        $('.overlay,#consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay,#consultation,#order,#thanks').fadeOut();
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click',function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay,#order').fadeIn();
+        })
+    });
 });
